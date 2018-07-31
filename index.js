13eb6b028d8ed5067a9e058aaa5c6940388317d4
Yu Li <yli0607x@gmail.com>
2018-07-31 18:11:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ function yourAnimal() {
   return animal
 }
 
-function add2(n) {
-  return n + two
+function add2('n') {
+  const two = 2
+  return 'n' + two
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
